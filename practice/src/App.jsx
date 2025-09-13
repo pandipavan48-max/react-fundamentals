35d5b3a862a0f4abc5ed769b96519e30e85ae11b
Pavan@42 <pandipavan48@gmail.com>
2025-09-13 12:32:15 +0530

    mirai

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,13 +1,22 @@
-import ProductCard from "./ProductCard.jsx"
-import Greetings from "./Greetings.jsx"
-function App() {
-  
-  return (
-    <>
-      <ProductCard />
-      <Greetings receivedName="John Doe" age={25} />
-    </>
-  )
-} 
+import ProductCard from "./ProductCard.jsx";
+import  "./ProductCard.css"
+import './App.css';
 
-export default App
+
+function App() {
+  const product = {
+   productname: " NAME:Pavan",
+    bloodgroup: " BLOOD GROUP:O+",
+    DOB: " DATE OF BIRTH:21/04/2005",
+    RollNO: " ROLL NO:23VV1A0542",
+
+
+  return (
+    <div>
+      <ProductCard productname={product.productname} bloodgroup={product.bloodgroup} DOB={product.DOB} RollNO={product.RollNO} photoUrl={product.photoUrl} />
+    </div>
+  );   
+}
+
+
+export default App;
--- practice/src/ProductCard.jsx
+++ practice/src/ProductCard.jsx
@@ -1,15 +1,16 @@
-function ProductCard() {
-  const productName = "Laptop";
-  const price = 49999;
-  const isAvailable = true;
+function ProductCard(app){
+  return(
 
-  return (
-    <div>
-      <p>Product Name: {productName}</p>
-      <p>Price: ₹{price}</p>
-      <p> {isAvailable === true ? "Available" : "Out of Stock"}</p>
+    
+    <div className="container">
+
+      <h1 className="title">{app.productname} </h1>
+      <h2 className="price">{app.bloodgroup}</h2>
+      <h3 className="date">{app.DOB}</h3>
+      <h4 className="name">{app.RollNO}</h4>
+      <img className="photo" src={app.photoUrl} alt="Student Image" />
     </div>
-  );
+  )
 }
 
-export default ProductCard;
+export default ProductCard
